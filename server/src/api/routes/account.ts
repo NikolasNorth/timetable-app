36e3f8e95b6068cc5ef7e1271a5c1d7c0a2bc3f9
Nikolas North <nikolasn09@gmail.com>
2020-11-24 14:27:27 -0500

    Create new accounts in POST /accounts

--- server/src/api/routes/account.ts
+++ server/src/api/routes/account.ts
@@ -1,11 +1,36 @@
 import {Router, Request, Response} from 'express';
-import {Account} from '../models/account';
+import {Account, IAccount} from '../models/account';
 
 export const router = Router();
 
+/** POST /v1/accounts */
 router.post('/', async (req: Request, res: Response) => {
     const name: string = req.body.name;
     const email: string = req.body.email;
     const password: string = req.body.password;
-    res.status(201).json({message: '200 OK.'})
+    try {
+        const account: IAccount | null = await Account.findOne({email: email}).exec();
+        if (account) {
+            res.status(400).json({
+                message: `An account has already been created for ${email}.`
+            });
+        } else {
+            let newAccount: IAccount = await Account.create({
+                name: name,
+                email: email,
+                password: password,
+                isAdmin: false,
+                isActive: true,
+                numSchedules: 0,
+                schedules: [],
+            });
+            newAccount = await newAccount.save();
+            res.status(201).json(newAccount);
+        }
+    } catch (err) {
+        console.error('Caught Error:', err);
+        res.status(500).json({
+            message: 'Internal server error.'
+        });
+    }
 });
