d92020f1fc67cc40aea074e42520ca159abd3de6
Nikolas North <nikolasn09@gmail.com>
2020-11-24 09:29:12 -0500

    Create Schedule model

---
+++ server/src/api/models/schedule.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import {Course} from './course';
+
+const scheduleSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    isPublic: {type: Boolean, required: true},
+    lastModified: {type: Date, required: true},
+    authorId: {type: Number, required: true},
+    size: {type: Number, required: false, default: 0},
+    courses: {type: [Course], required: false, default: []},
+});
+
+export const Schedule = mongoose.model('Schedule', scheduleSchema);
