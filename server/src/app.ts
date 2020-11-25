dd3723a19acd61c94d576cb5578638b3d1bca058
Nikolas North <nikolasn09@gmail.com>
2020-11-25 16:50:15 -0500

    Create nodemailer transporter

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,8 +4,18 @@ import {Config} from './etc/config';
 import bodyParser from 'body-parser';
 import {requestLogger, preventCorsErrors} from "./app.middleware";
 import {router as accountRouter} from './api/routes/account';
+import * as nodemailer from 'nodemailer';
 
 export const app: Application = express();
+export const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: Config.email.user,
+        pass: Config.email.password,
+    }
+});
 
 const dbUrl: string = `mongodb://127.0.0.1:27017/${Config.db.name}`
 // const dbUrl = `mongodb+srv://${Config.db.user}:${Config.db.password}@timetable-api.xmw9o.mongodb.net/${Config.db.name}?retryWrites=true&w=majority`;
