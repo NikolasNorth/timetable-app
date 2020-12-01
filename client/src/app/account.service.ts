c5b0901cfeb2ce0eadd7c5306217300c42686968
Nikolas North <nikolasn09@gmail.com>
2020-12-01 09:53:51 -0500

    Remove unused import

--- client/src/app/account.service.ts
+++ client/src/app/account.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Account} from './@types/account';
 import {Observable} from 'rxjs';
-import {AuthService} from './auth.service';
 
 @Injectable({
   providedIn: 'root'
