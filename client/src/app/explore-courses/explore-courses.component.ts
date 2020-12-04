8bfe26dab6665efd306be8f052379de2b9c34346
Nikolas North <nikolasn09@gmail.com>
2020-12-04 11:49:19 -0500

    Create starter template for explore courses

--- client/src/app/explore-courses/explore-courses.component.ts
+++ client/src/app/explore-courses/explore-courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Course} from '../@types/course';
 
 @Component({
   selector: 'app-explore-courses',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./explore-courses.component.scss']
 })
 export class ExploreCoursesComponent implements OnInit {
+  results: Course[];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  searchCourses(title: string, subject: string, code: string, component: string): void {
+    const course: Course = {
+      _id: "1",
+      subject: subject,
+      code: code,
+      title: title,
+      classSection: "001",
+      startTime: "12:00 PM",
+      endTime: "1:00 PM",
+      days: [],
+      rating: 5.0,
+      reviews: [],
+    };
+    if (!this.results) this.results = [];
+    this.results.push(course);
+  }
 }
